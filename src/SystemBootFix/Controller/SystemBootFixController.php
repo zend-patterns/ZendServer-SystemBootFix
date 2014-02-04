68de70c410ac3e42a9ab1445e399bed69025894b
yonni <yonni.m@zend.com>
2014-02-04 11:56:27 +0200

    Retrieve and display snapshot profile

--- src/SystemBootFix/Controller/SystemBootFixController.php
+++ src/SystemBootFix/Controller/SystemBootFixController.php
@@ -10,16 +10,44 @@
 namespace SystemBootFix\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use ZendServer\FS\FS;
+use Configuration\Controller\WebAPIController;
 
 class SystemBootFixController extends AbstractActionController
 {
-    public function indexAction()
-    {
-        return array();
+    public function indexAction() {
+    	$snapshotsMapper = $this->getServiceLocator()->get('Snapshots\Db\Mapper');
+    	
+    	$snapshot = $snapshotsMapper->findSystemSnapshot();
+    	$snapshotId = $snapshot->getId();
+    	if (! $snapshotId) {
+    		throw new \Exception("Failed retrieving the bootStrap snapshot");
+    	}
+    		
+    	$data = $snapshot->getData();
+    	$filePath = FS::getGuiTempDir() . DIRECTORY_SEPARATOR . 'systemBoot.zip';
+    	file_put_contents($filePath, $data);
+    	
+    	$archive = FS::getZipArchive($filePath, 0);
+    	
+    	if ($archive->locateName(WebAPIController::METADATA_FILE) !== false) {
+	    	$snapshotProfile = $archive->getFromName(WebAPIController::METADATA_FILE);
+    	} else {
+    		$snapshotProfile = null;
+    	}
+    	
+    	$archive->close();
+    	
+    	unlink($filePath);
+    	
+    	
+    	
+        return array(
+        	'profileToImport' => $snapshotProfile
+        );
     }
     
-    public function patchAction()
-    {
+    public function patchAction() {
     	return $this->redirect()->toRoute('default',array('controller' => 'SystemBootFix', 'action' => 'index'));
     }
 
